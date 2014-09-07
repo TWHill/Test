6dff15819efcf96c46ef7bea717b304e67f14463
TWHill <tim.hill.83@gmail.com>
2014-09-07 11:58:53 +0100

    Hello World 2

--- GitTestApp/GitTestApp/Program.cs
+++ GitTestApp/GitTestApp/Program.cs
@@ -10,7 +10,7 @@ namespace GitTestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World 2");
             Console.ReadLine();
         }
     }

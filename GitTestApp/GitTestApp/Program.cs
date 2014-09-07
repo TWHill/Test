57d2c33927b1e2849d30d448c521fc1d35cedbbc
Kieranties <Kieran@Kieranties.com>
2014-09-07 12:30:42 +0100

    Hello Tim

--- GitTestApp/GitTestApp/Program.cs
+++ GitTestApp/GitTestApp/Program.cs
@@ -10,9 +10,7 @@ namespace GitTestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-			Console.WriteLine("Hello World 2");
-            Console.WriteLine("Hello World 3");
+            Console.WriteLine("Hello Tim");
             Console.ReadLine();
         }
     }

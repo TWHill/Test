85b77b9ecfc46b35835794cd13c7789fa4dfd2b9
TWHill <tim.hill.83@gmail.com>
2014-09-07 12:29:14 +0100

    "Fixed" HelloWorld

--- GitTestApp/GitTestApp/Program.cs
+++ GitTestApp/GitTestApp/Program.cs
@@ -10,8 +10,6 @@ namespace GitTestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-			Console.WriteLine("Hello World 2");
             Console.WriteLine("Hello World 3");
             Console.ReadLine();
         }

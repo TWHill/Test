5b9f4132b6f5d1e52919b0d6ecea025d245ee102
Kieranties <Kieran@Kieranties.com>
2014-09-07 12:20:34 +0100

    New Hello world

--- GitTestApp/GitTestApp/Program.cs
+++ GitTestApp/GitTestApp/Program.cs
@@ -10,7 +10,9 @@ namespace GitTestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World 2");
+            Console.WriteLine("Hello World");
+			Console.WriteLine("Hello World 2");
+            Console.WriteLine("Hello World 3");
             Console.ReadLine();
         }
     }
